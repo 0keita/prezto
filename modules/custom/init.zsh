8b562cde00dc479e045297ab4d2a51c5783ffeec
Keita YOSHIDA <yoshida_keita@kakaku.com>
2018-11-08 12:00:50 +0900

    Enable to use peco command in linux env

--- modules/custom/init.zsh
+++ modules/custom/init.zsh
@@ -8,9 +8,13 @@ alias ll='ls -lha'
 
 # Peco
 function peco-history-selection() {
+  if (( $+commands[tac] )); then
+    BUFFER=`history -n 1 | tac  | awk '!a[$0]++' | peco`
+  else
     BUFFER=`history -n 1 | tail -r  | awk '!a[$0]++' | peco`
-    CURSOR=$#BUFFER
-    zle reset-prompt
+  fi
+  CURSOR=$#BUFFER
+  zle reset-prompt
 }
 
 zle -N peco-history-selection
